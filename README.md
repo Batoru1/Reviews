3ce414b59bb02ede26dcdd18687e21c827c104a1
batoru <tbakanas2@gmail.com>
2023-04-30 11:38:38 +0300

    before break commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Reviews
+
 Reviews ipsum corporation site
+
+finished and cleaned html an css. Used ::after, ::before, position:relative/absolute methods
---
+++ app.js
@@ -0,0 +1,32 @@
+//reviev data
+const reviews = [
+  {
+    id: 1,
+    name: 'kitty kitington',
+    job: 'supreme ruler',
+    img: 'images/cat.jpg',
+    text: "My water bowl is clean and freshly replenished, so i'll drink from the toilet grab pompom in mouth and put in water dish yet pretend you want to go out but then don't naughty running cat. Fall asleep on the washing machine eats owners hair then claws head stare at owner accusingly",
+  },
+  {
+    id: 2,
+    name: 'chuck just chuck',
+    job: 'hr manager',
+    img: 'images/chuck.jpeg',
+    text: "Chuck Norris doesn't have feet, just another pair of fists Chuck is the reason you can't leave the Hotel California, The Great Wall of China was originally created to keep Chuck Norris out. It failed miserably. Chuck Norris has two speeds. Walk, and Kill.",
+  },
+  {
+    id: 3,
+    name: 'perpetum internum',
+    job: 'le workee',
+    img: 'images/dank.png',
+    text: 'Heat map difficulty Super Mario attract mode Battlefield dead zone corruptor hyperspace Ratchet & Clank controller zero-player game. Rubber banding Camping/Active Camping Grand Theft Auto III frame-rate indie QQ Platform Game exploit DLC ',
+  },
+  {
+    id: 4,
+    name: 'sam "the man"',
+    job: 'talent aquisition specialist',
+    img: 'images/samuel.jpg',
+    text: "Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.",
+  },
+];
+//select item
